95fe5982d7e47c776be0e50664e649258d0a3006
ruanrls <ruanrafaelleite@gmail.com>
2026-03-04 21:52:53 -0300

    refactor: streamline command initialization in DEFAULT_COMMANDS using array and iterators

--- libs/snapr/src/configuration/defaults.rs
+++ libs/snapr/src/configuration/defaults.rs
@@ -3,127 +3,30 @@ use std::{collections::HashMap, sync::LazyLock};
 use crate::commands::{Command, CommandHash, KeyBinding, ScreenPositions};
 
 pub(crate) static DEFAULT_COMMANDS: LazyLock<CommandHash> = LazyLock::new(|| {
-    let mut command_storage: CommandHash = HashMap::new();
+    let defaults = [
+        (4, 39, ScreenPositions::Right),
+        (4, 38, ScreenPositions::Top),
+        (4, 40, ScreenPositions::Bottom),
+        (4, 37, ScreenPositions::Left),
+        (4, 73, ScreenPositions::TopLeft),
+        (4, 79, ScreenPositions::TopRight),
+        (4, 75, ScreenPositions::BottomLeft),
+        (4, 76, ScreenPositions::BottomRight),
+        (4, 67, ScreenPositions::Center),
+        (4, 13, ScreenPositions::Maximize),
+    ];
 
-    let key = KeyBinding {
-        modifiers: 4,
-        key: 39,
-    };
-    command_storage.insert(
-        key,
-        Command {
-            key_binding: key,
-            position: ScreenPositions::Right,
-        },
-    );
-
-    let key = KeyBinding {
-        modifiers: 4,
-        key: 38,
-    };
-    command_storage.insert(
-        key,
-        Command {
-            key_binding: key,
-            position: ScreenPositions::Top,
-        },
-    );
-
-    let key = KeyBinding {
-        modifiers: 4,
-        key: 40,
-    };
-    command_storage.insert(
-        key,
-        Command {
-            key_binding: key,
-            position: ScreenPositions::Bottom,
-        },
-    );
-
-    let key = KeyBinding {
-        modifiers: 4,
-        key: 37,
-    };
-    command_storage.insert(
-        key,
-        Command {
-            key_binding: key,
-            position: ScreenPositions::Left,
-        },
-    );
-
-    let key = KeyBinding {
-        modifiers: 4,
-        key: 73,
-    };
-    command_storage.insert(
-        key,
-        Command {
-            key_binding: key,
-            position: ScreenPositions::TopLeft,
-        },
-    );
-
-    let key = KeyBinding {
-        modifiers: 4,
-        key: 79,
-    };
-    command_storage.insert(
-        key,
-        Command {
-            key_binding: key,
-            position: ScreenPositions::TopRight,
-        },
-    );
-
-    let key = KeyBinding {
-        modifiers: 4,
-        key: 75,
-    };
-    command_storage.insert(
-        key,
-        Command {
-            key_binding: key,
-            position: ScreenPositions::BottomLeft,
-        },
-    );
-
-    let key = KeyBinding {
-        modifiers: 4,
-        key: 76,
-    };
-    command_storage.insert(
-        key,
-        Command {
-            key_binding: key,
-            position: ScreenPositions::BottomRight,
-        },
-    );
-
-    let key = KeyBinding {
-        modifiers: 4,
-        key: 67,
-    };
-    command_storage.insert(
-        key,
-        Command {
-            key_binding: key,
-            position: ScreenPositions::Center,
-        },
-    );
-
-    let key = KeyBinding {
-        modifiers: 4,
-        key: 13,
-    };
-    command_storage.insert(
-        key,
-        Command {
-            key_binding: key,
-            position: ScreenPositions::Maximize,
-        },
-    );
-
-    command_storage
+    defaults
+        .into_iter()
+        .map(|(modifiers, key, position)| {
+            let key_binding = KeyBinding { modifiers, key };
+            (
+                key_binding,
+                Command {
+                    key_binding,
+                    position,
+                },
+            )
+        })
+        .collect()
 });
